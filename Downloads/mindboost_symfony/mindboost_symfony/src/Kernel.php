e475afd5dde23392d561cf4d456ba505c60c1f0a
nada <nada.ajmi@esprit.tn>
2026-04-19 16:31:48 +0100

    fix: update Kernel.php with latest changes

--- Downloads/mindboost_symfony/mindboost_symfony/src/Kernel.php
+++ Downloads/mindboost_symfony/mindboost_symfony/src/Kernel.php
@@ -8,4 +8,10 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function __construct(string $environment, bool $debug)
+    {
+        (new \Symfony\Component\Dotenv\Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+        parent::__construct($environment, $debug);
+    }
 }
